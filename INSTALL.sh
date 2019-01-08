9a0e388f4d152594c5f7537ebda18851fa4d2c3e
Jordan Baczuk <Jordan.Baczuk@gmail.com>
2019-01-08 07:38:28 -0700

    rename base58 and update INSTALL script

--- INSTALL.sh
+++ INSTALL.sh
@@ -2,4 +2,10 @@ mv blockchain_fundamentals.py ../lib/python3.6
 cp *.py ../bin
 cp sample.env ../lib/python3.6
 cp coins -r ../lib/python3.6
-cd ../
+cd ../bin
+
+for file in *.py; do
+    mv -- "$file" "${file%%.py}"
+done
+
+cd ../../
