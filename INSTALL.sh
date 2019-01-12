964b7f8fb59c4968ca1c307236918c4c29836469
Jordan Baczuk <Jordan.Baczuk@gmail.com>
2019-01-12 13:47:43 -0700

    fix INSTALL script

--- INSTALL.sh
+++ INSTALL.sh
@@ -1,7 +1,7 @@
 mv blockchain_fundamentals.py ../lib/python3.6
 cp *.py ../bin
 cp -R coins ../lib/python3.6/
-echo "COIN='bitcoin'" >> ../bin/activate
+echo "export COIN='bitcoin'" >> ../bin/activate
 cd ../bin
 
 for file in *.py; do
